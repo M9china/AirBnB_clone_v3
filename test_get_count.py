ca55a6e830f0e79618fa144ec4457c47ab83014c
M9china <nqobilencaza2@gmail.com>
2024-02-17 10:09:39 +0200

    error handling

--- test_get_count.py
+++ test_get_count.py
@@ -4,8 +4,16 @@
 from models import storage
 from models.state import State
 
+# Count all objects and State objects
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+# Get the list of State objects
+state_objects = list(storage.all(State).values())
+
+if state_objects:
+    # Check if the list is not empty before accessing the first element
+    first_state_id = state_objects[0].id
+    print("First state: {}".format(storage.get(State, first_state_id)))
+else:
+    print("No State objects found.")
